c780e33f81b9395bb8aa0f9c246803abe533f03c
Subroto Das <rupashi.bangla.club@gmail.com>
2024-03-19 00:36:36 +0600

    Initial Setup

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 import app from './app';
 import config from './app/config';
 import mongoose from 'mongoose';
@@ -7,10 +8,11 @@ async function bootstrap() {
   try {
     //Moongoose Connections
     await mongoose.connect(`${config.database_url as string}`);
-
+      console.log("Mongodb Connection Successfully")
     //App Listener
     app.listen(config.port, () => {
       console.log(`server is running at http://localhost:${config.port}`);
+      console.log("Mongodb Connection Successfully")
     });
   } catch (error) {
     console.log(error);
